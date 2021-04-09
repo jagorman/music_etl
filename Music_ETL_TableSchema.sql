4fdf46b8090887615536318723d6c3c99b0486d7
JA Gorman <jagorman42@gmail.com>
2021-04-08 22:11:39 -0500

    added sql file and engine connect

---
+++ Music_ETL_TableSchema.sql
@@ -0,0 +1,16 @@
+CREATE TABLE "year_db" (
+    "genres" varchar(50) NOT NULL,
+    "acousticness" decimal(4,3)    NOT NULL,
+    "danceability" decimal(4,3) NOT NULL,
+    "duration_ms" decimal() NOT NULL,
+    "energy" decimal(2) NOT NULL,
+    "instrumentalness" decimal(2) NOT NULL,
+    "liveness" decimal(2) NOT NULL,
+    "loudness" decimal(2)OT NULL,
+    "speechiness" decimal(2) NOT NULL,
+    "tempo" decimal(2) NOT NULL,
+    "valence" decimal(2) NOT NULL
+    CONSTRAINT "" PRIMARY KEY (
+        "Incident_ID"
+     )
+);
